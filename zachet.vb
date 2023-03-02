dd78833c9fc2b2a88997ecd7efe25f8bd7f02c4f
AnastasiaPenzina21IS <123143991+AnastasiaPenzina21IS@users.noreply.github.com>
2023-03-02 09:35:15 +0300

    Add files via upload

--- zachet.vb
+++ zachet.vb
@@ -2,7 +2,7 @@ Imports System
 
 Module zachet
     Private brush As Char = "0"c
-    Dim num01 As Byte, num03 As Byte
+    Dim num01 As Byte
     Dim x As Integer, y As Integer, x1 As Integer, y1 As Integer
     Sub Main(args As String())
 
@@ -12,48 +12,36 @@ Module zachet
 
         Select Case num01
             Case 1
-                If num03 = 1 Then
-                    horizon(x, y, x1)
-                Else
-                    horizon(5, 5, 15)
-                End If
+
+                horizon(5, 5, 15)
 
             Case 2
-                If num03 = 1 Then
-                    vertically(x, y, x1)
-                Else
-                    vertically(5, 5, 15)
-                End If
+
+                vertically(5, 5, 15)
 
             Case 3
-                If num03 = 1 Then
-                    closcube(x, y, x1, y1)
-                Else
-                    closcube(5, 5, 50, 40)
-                End If
+
+                closcube(5, 5, 50, 40)
 
             Case 4
-                If num03 = 1 Then
-                    empcube(x, y, x1, y1)
-                Else
-                    empcube(10, 10, 20, 30)
-                End If
+
+                empcube(10, 10, 20, 30)
 
             Case 5
-                If num03 = 1 Then
-                    grid(x, y, x1, y1)
-                Else
-                    grid(10, 10, 40, 7)
-                End If
+
+                grid()
 
             Case 6
-                ornament(10, 5, 30)
+
+                ornament()
 
             Case 7
-                lines(10, 5, 30)
+
+                lines()
 
             Case 8
-                colorOrnament(10, 5, 30)
+
+                colorOrnament()
 
         End Select
     End Sub
@@ -78,59 +66,40 @@ Module zachet
     Sub closcube(leftx As Byte, topy As Byte, width As Byte, height As Byte)
         Console.SetCursorPosition(leftx, topy)
 
-        For j = 1 To height
-            For i = 1 To width
-                Console.Write(brush)
-            Next
-            Console.SetCursorPosition(leftx, topy + j)
+        For j = 1 To 10
+            horizon(5, 5 + j, 20)
         Next
 
     End Sub
     Sub empcube(leftx As Byte, topy As Byte, width As Byte, height As Byte)
         Console.SetCursorPosition(leftx, topy)
 
-        For i = 1 To width
-            Console.Write(brush)
-        Next
-
-        For j = 1 To height - 3
-            Console.SetCursorPosition(leftx, topy + j)
-            Console.Write(brush)
-            Console.SetCursorPosition(leftx + width - 1, topy + j)
-            Console.WriteLine(brush)
-        Next
-
-        Console.SetCursorPosition(leftx, topy + height - 2)
-        For i = 1 To width
-            Console.Write(brush)
-        Next
+        horizon(10, 25, 40)
+        vertically(10, 5, 2)
+        horizon(10, 5, 40)
+        vertically(50, 5, 21)
 
     End Sub
-    Sub grid(leftx As Byte, topy As Byte, width As Integer, kletki As Byte)
-        Console.SetCursorPosition(leftx, topy)
-
-        For i = 2 To width
-            Console.Write(brush)
-        Next
+    Sub grid()
 
         For i = 1 To 8
-            horizon(2 + 7, 2 + i * 3, 50)
-            vertically(2 + 7 * i, 2 + 3, 27)
+            horizon(2 + 10, 2 + i * 5, 70)
+            vertically(2 + 10 * i, 2 + 5, 36)
         Next
 
-        Console.SetCursorPosition(10, 10)
     End Sub
 
-    Sub ornament(leftX As Byte, topY As Byte, length As Byte)
+    Sub ornament()
 
         For j = 1 To 5
             For i = 1 To 5
                 closcube(7 + 10 * i, 5 + j * 10, 7, 7)
             Next
         Next
+
     End Sub
 
-    Sub lines(leftX As Byte, topY As Byte, length As Byte)
+    Sub lines()
 
         For i = 1 To 6
             horizon(10, 5 * i, 40)
@@ -138,7 +107,7 @@ Module zachet
 
     End Sub
 
-    Sub colorOrnament(leftX As Byte, topY As Byte, length As Byte)
+    Sub colorOrnament()
         Console.ForegroundColor = ConsoleColor.Black
         Console.BackgroundColor = ConsoleColor.White
 
